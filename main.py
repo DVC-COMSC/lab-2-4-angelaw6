f0ca5fb042e2f9a11cacc4526e36331253408472
AngelaWei <aw117964@gmail.com>
2023-06-28 11:36:33 -0700

    Lab 2-4

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     # Comlete your code here
     ##################################################
 
+    sub1 = original_str.split(' ')[0]
+    sub2 = original_str.split(' ')[1]
+    merged_str = '%s %s' %(sub2, sub1)
+
     print(sub2)
     print(sub1)
     print(merged_str)
